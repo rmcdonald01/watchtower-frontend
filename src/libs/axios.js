820e5dd6dd82f7f4dd55522f3ffb47d4ab9d7f88
Ramon <mrramon101@gmail.com>
2023-03-12 20:31:29 -0500

    Update api url

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:881/' : 'https://mywatchtower.gq/',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:881/' : 'https://api.watchtower.objective-solutions.com/',
   // In the event that I want to test with another device on my local network
   // baseURL: process.env.NODE_ENV === 'development' ? 'http://192.168.0.8:881/' : 'https://mywatchtower.gq/',
   // timeout: 1000,
